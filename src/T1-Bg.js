a171cdf81e9e2ee21d319a12824ae90847ffc008
Yankov Vlad <vladankov996@gmail.com>
2021-07-01 17:03:13 +0300

    feat: add t1 solution

---
+++ src/T1-Bg.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const Bg = ({ingredient}) => {
+
+    return (
+        <div className={ingredient}></div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Pizza from './T1-Pizza'
 
-import reportWebVitals from './reportWebVitals';
+
+
 
 ReactDOM.render(
   <React.StrictMode>
-
+      <Pizza />
   </React.StrictMode>,
   document.getElementById('root')
 );
